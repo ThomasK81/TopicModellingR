1a99d6ed64c980c3ee2c018403107368e9f5840c
Thomas Koentges <thomas.koentges@uni-leipzig.de>
2015-10-19 16:29:02 +0200

    Arabic updated

--- ArabicTM/ArabicTM.r
+++ ArabicTM/ArabicTM.r
@@ -1,209 +1,40 @@
-setwd("~/OneDrive/ArabicTM")
+setwd("~/Dropbox/ArabicTM")
 # libraries needed
 
 library(tm)
-library(XML)
-library(RCurl)
 library(plyr)
 library(lda)
 library(LDAvis)
 
-# read in some stopwords:
-
-# stop_words1 <- stopwords("SMART")
-stopwords_latin <- c("ab", "ac", "ad", "adhic", "aliqui", "aliquis", "an", "ante", "apud", "at", "atque", "aut", "autem", "cum", "cur", "de", "deinde", "dum", "ego", "enim", "ergo", "es", "est", "et", "etiam", "etsi", "ex", "fio", "haud", "hic", "iam", "idem", "igitur", "ille", "in", "infra", "inter", "interim", "ipse", "is", "ita", "magis", "modo", "mox", "nam", "ne", "nec", "necque", "neque", "nisi", "non", "nos", "o", "ob", "per", "possum", "post", "pro", "quae", "quam", "quare", "qui", "quia", "quicumque", "quidem", "quilibet", "quis", "quisnam", "quisquam", "quisque", "quisquis", "quo", "quoniam", "sed", "si", "sic", "sive", "sub", "sui", "sum", "super", "suus", "tam", "tamen", "trans", "tu", "tum", "ubi", "uel", "uero", "ut", "t", "cos2", "coepio", "sum", "edo")
-stopwords_greek <- c("μή", "ἑαυτοῦ", "ἄν", "ἀλλ’", "ἀλλά", "ἄλλοσ", "ἀπό", "ἄρα", "αὐτόσ", "δ’", "δέ", "δή", "διά", "δαί", "δαίσ", "ἔτι", "ἐγώ", "ἐκ", "ἐμόσ", "ἐν", "ἐπί", "εἰ", "εἰμί", "εἴμι", "εἰσ", "γάρ", "γε", "γα^", "ἡ", "ἤ", "καί", "κατά", "μέν", "μετά", "μή", "ὁ", "ὅδε", "ὅσ", "ὅστισ", "ὅτι", "οὕτωσ", "οὗτοσ", "οὔτε", "οὖν", "οὐδείσ", "οἱ", "οὐ", "οὐδέ", "οὐκ", "περί", "πρόσ", "σύ", "σύν", "τά", "τε", "τήν", "τῆσ", "τῇ", "τι", "τί", "τισ", "τίσ", "τό", "τοί", "τοιοῦτοσ", "τόν", "τούσ", "τοῦ", "τῶν", "τῷ", "ὑμόσ", "ὑπέρ", "ὑπό", "ὡσ", "ὦ", "ὥστε", "ἐάν", "παρά", "σόσ")
-# stop_words3 <- c()
-
-# Decide which set of stopwords
-
-stop_words <- stopwords_latin
-
-# Import corpus from CTS repository
-
-# baseURL <- "http://www.perseus.tufts.edu/hopper/CTS?request=GetPassage&urn="
-# requestURN <- "urn:cts:latinLit:phi0448.phi001"
-# requestURN <- "urn:cts:greekLit:tlg0003.tlg001"
-# Fetch Reffs for CTS Repository
-
-# t1 <- Sys.time()
-
-# reffURL <- "http://www.perseus.tufts.edu/hopper/CTS?request=GetValidReff&urn="
-# message("Retrieve Reffs for ", requestURN)
-# URL <- paste(reffURL, requestURN, sep = "")
-# URLcontent <- getURLContent(URL)
-# reffs <- unlist(strsplit(URLcontent, split="<urn>|</urn>"))
-# reffs <- reffs[2:length(reffs)]
-# reffs <- reffs[seq(1, length(reffs), 2)]
-
-# t2 <- Sys.time()
-# Reff_time <- t2 - t1
-
-# Fetch Text from CTS Repository
-
-# t1 <- Sys.time()
-
-# output_list <- list()
-
-# for (i in reffs[1:100]) {
-# message("Retrieve section ", i)
-# URL <- paste(baseURL, i, sep = "")
-# message("Fetching ", URL)
-# URLcontent <- getURLContent(URL)
-# dum <- xmlParse(URLcontent)
-
-# Parse the XML and extract needed information. 
-
-XMLpassage <-function(xdata){
-    dumFun <- function(x){
-        xname <- xmlName(x)
-        xattrs <- xmlAttrs(x)
-        c(sapply(xmlChildren(x), xmlValue), name = xname, xattrs)
-    }
-    dum <- xmlParse(xdata)
-    as.data.frame(t(xpathSApply(dum, "//*/tei:body", dumFun)), stringsAsFactors = FALSE)
-}
-
-# output_list[[i]] <- XMLpassage(URLcontent)
-# message("---------------------------------------")
-}
-
-# t2 <- Sys.time()
-# Fetch_time <- t2 - t1
-
-# Build corpus
-
-# corpus <- do.call("rbind",output_list) #combine all vectors into a matrix
-# corpus <- unique(corpus) # returns the unique rows of catalogue.
-
-# Save corpus to disk
-
-# write.table(corpus, file = 'corpus.csv', append = FALSE, quote = FALSE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = TRUE, col.names = TRUE)
-
-# Build base for topic modelling
-
-
-# research_corpus <- corpus[,"div"]
-# research_corpus <- factor(research_corpus)output_names <- read.csv("CartoonSep2013/CartoonSep2013.csv")$identifier
-# output_names <- rownames(corpus)
+# Read corpus
 
 base_corpus <- read.table("Shamela_0035100.csv", sep="\t", header=FALSE)
-research_corpus <- base_corpus[["V2"]]
-output_names <- base_corpus[["V1"]]
+research_corpus <- as.character(base_corpus[["V2"]])
+output_names <- as.character(base_corpus[["V1"]])
 
-# pre-processing:
-
-# research_corpus <- tolower(research_corpus)  # force to lowercase
-# research_corpus <- gsub("'", " ", research_corpus)  # remove apostrophes
-# research_corpus <- gsub("-", "", research_corpus)  # remove hyphens, create composita
-# research_corpus <- gsub("v", "u", research_corpus) # normalise to 'u'
-# research_corpus <- gsub("j", "i", research_corpus) # normalise to 'i'
-
-research_corpus <- gsub("[[:punct:]]", " ", research_corpus)  # replace punctuation with space
-research_corpus <- gsub("[[:cntrl:]]", " ", research_corpus)  # replace control characters with space
-research_corpus <- gsub("^[[:space:]]+", "", research_corpus) # remove whitespace at beginning of documents
-research_corpus <- gsub("[[:space:]]+$", "", research_corpus) # remove whitespace at end of documents
-research_corpus <- gsub("[0-9]", "", research_corpus) #remove numbers
-
-###
-# research_corpus <- gsub(" sum ", " ", research_corpus)  # remove esse
-# research_corpus <- gsub(" es ", " ", research_corpus)  # remove esse
-# research_corpus <- gsub(" est ", " ", research_corpus)  # remove esse
-# research_corpus <- gsub(" sumus ", " ", research_corpus)  # remove esse
-# research_corpus <- gsub(" estis ", " ", research_corpus)  # remove esse
-# research_corpus <- gsub(" sunt ", " ", research_corpus)  # remove esse
-# research_corpus <- gsub(" esse ", " ", research_corpus)  # remove esse
-research_corpus <- gsub(" cum ", " ", research_corpus)  # remove cum
-
-# produce dictionary for stemming:
-t1 <- Sys.time()
-
-corpus_words <- unique(unlist(strsplit(research_corpus, "\\W+", perl=TRUE)))
-corpus_words <- sort(corpus_words)
-
-#function for stemming
-
-parsing <- function(x){
-URL <- paste("http://www.perseus.tufts.edu/hopper/xmlmorph?lang=lat&lookup=", x, sep = "")
-message("Accessing ", URL)
-
-XMLpassage <-function(xdata){
-    miner <- function(x){
-        xname <- xmlName(x)
-        xattrs <- xmlAttrs(x)
-        c(sapply(xmlChildren(x), xmlValue), name = xname, xattrs)
-    }
-    result <- xmlParse(xdata)
-    temp.df <- as.data.frame(t(xpathSApply(result, "//*/lemma", miner)), stringsAsFactors = FALSE)
-    as.vector(temp.df[['text']])
-}
-
-URLcontent <- tryCatch(
-{
-getURLContent(URL)
-}, 
-error = function(err) 
-{
-message(x, " -query caused server error. Return original value.")
-lemma <- x
-return(lemma)
-})
-
-lemma <- tryCatch(
-{
-XMLpassage(URLcontent)
-}, 
-error = function(err) 
-{
-message(x, " not found. Return original value.")
-lemma <- x
-return(lemma)
-})
-lemma <- gsub("[0-9]", "", lemma)
-lemma <- tolower(lemma)
-lemma <- unique(lemma)
-lemma <- paste(lemma, sep="", collapse="_")
-if (nchar(lemma) == 0) lemma <- x
-message(x, " is ", lemma)
-return(lemma)}
-
-correcting <- function(x){
-object <- unlist(x)
-# corrected <- mapvalues(object, from=names(stem_dictionary), to=tolower(stem_dictionary), warn_missing = FALSE)
-corrected <- mapvalues(object, from=tolower(original), to=tolower(new), warn_missing = FALSE)
-corrected <- paste(corrected, collapse=" ")
-return(corrected)}
-
-#stemming
-# stem_dictionary <- sapply(corpus_words, parsing)
-# write.table(stem_dictionary, file = 'stem_dictionary1.csv', append = FALSE, quote = FALSE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = TRUE, col.names = TRUE)
-# write.table(as.data.frame(number_lemmata), file = 'stem_overview.csv', append = FALSE, quote = FALSE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = TRUE, col.names = TRUE)
-
-# NumberOfForms <- max(unique(sapply(stem_dictionary, length)))
-
-
-# stem_dictionary <- read.table("stem_dictionary_ultimate.csv", sep = " ")
-
-# number_lemmata <- sapply(stem_dictionary, length)
-
-# stem_dictionary <- as.data.frame(stem_dictionary)
-# original <- rownames(stem_dictionary)
-# new <- as.character(stem_dictionary[,1])
-
-# temp <- strsplit(research_corpus, " ")
-# temp_correct <- lapply(temp, correcting) 
-# research_corpus <- unlist(temp_correct)
-# t2 <- Sys.time()
-# correcting_time <- t2 - t1
-
-###
+# pre-processing goes here
 
 # tokenize on space and output as a list:
 doc.list <- strsplit(research_corpus, "[[:space:]]+")
 
+# produce dictionary for stemming:
+
+all_words <- unlist(doc.list)
+corpus_words <- unique(all_words)
+corpus_words <- sort(all_words)
+
+#function for stemming goes here
+
+#stemming
+
 # compute the table of terms:
 term.table <- table(unlist(doc.list))
 term.table <- sort(term.table, decreasing = TRUE)
+
+# determing stopwords
+
+stop_words <- as.data.frame(term.table)
+stop_words <- row.names(as.data.frame(stop_words[1:100,]))
 
 # remove terms that are stop words or occur fewer than "occurenses" times:
 occurences <- 3
@@ -227,8 +58,8 @@ N <- sum(doc.length)  # total number of tokens in the data (546,827)
 term.frequency <- as.integer(term.table)  # frequencies of terms in the corpus [8939, 5544, 2411, 2410, 2143, ...]
 
 # MCMC and model tuning parameters:
-K <- 12
-G <- 5000
+K <- 20
+G <- 500
 alpha <- 0.02
 eta <- 0.02
 
@@ -261,7 +92,7 @@ json <- createJSON(phi = research_corpusAbstracts$phi,
                    doc.length = research_corpusAbstracts$doc.length, 
                    vocab = research_corpusAbstracts$vocab, 
                    term.frequency = research_corpusAbstracts$term.frequency,
-			 R=40)
+                   R=50)
 			 
 #Visulise and start browser
 serVis(json, out.dir = 'Arabic_vis', open.browser = FALSE)
