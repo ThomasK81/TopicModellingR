063755f7dfe03a6d6fa3f4905fe6439ce4d0dfa4
Thomas Koentges <thomas.koentges@uni-leipzig.de>
2015-11-13 09:29:59 +0100

    update araTM

--- ArabicTM/ArabicTM.r
+++ ArabicTM/ArabicTM.r
@@ -39,7 +39,7 @@ output_names <- as.character(base_corpus[["V1"]])
 
 ## reduce sample
 
-research_corpus <- research_corpus[1:10]
+research_corpus <- research_corpus[1:5]
 
 # pre-processing goes here
 
@@ -60,7 +60,7 @@ corpus_words <- sort(corpus_words)
 
 parsing2 <- function(x){
   URL <- paste(morpheusURL2, x, "&lang=ara&engine=aramorph", sep = "")
-  message("Accessing ", URL)
+  message(round((match(x, corpus_words)-1)/length(corpus_words)*100, digits=2), "% processed. Checking ", x," now.")
   
   XMLpassage <-function(xdata){
     miner <- function(x){
@@ -94,14 +94,14 @@ parsing2 <- function(x){
   lemma <- unique(lemma)
   # lemma <- paste(lemma, sep="", collapse="_")
   if (nchar(lemma) == 0) lemma <- x
-  message(x, " is ", lemma)
+  message(x, " is ", paste(lemma, sep="", collapse=" or "))
   return(lemma)}
 
 ## translating
 
 translating <- function(x){
   URL <- paste(morpheusURL2, x, "&lang=ara&engine=aramorph", sep = "")
-  message("Accessing ", URL)
+  message(round((match(x, corpus_words)-1)/length(corpus_words)*100, digits=2), "% processed. Checking ", x," now.")
   
   XMLpassage <-function(xdata){
     miner <- function(x){
@@ -135,7 +135,7 @@ translating <- function(x){
   lemma <- unique(lemma)
   # lemma <- paste(lemma, sep="", collapse="_")
   if (nchar(lemma) == 0) lemma <- x
-  message(x, " is ", lemma)
+  message(x, " is ", paste(lemma, sep="", collapse=" or "))
   return(lemma)}
 
 ## lemmatiser function
@@ -184,6 +184,7 @@ for (i in 1:length(corrected_corpus)) {
   corrected_corpus[[i]] <- paste(unlist(corrected_corpus[[i]]), collapse=" ")
 }
 
+translation_corpus <- research_corpus
 research_corpus <- unlist(corrected_corpus)
 
 doc.list <- strsplit(research_corpus, "[[:space:]]+")
@@ -272,3 +273,131 @@ colnames(theta.frame) <- paste("topic", as.character(1:K-1), sep="")
 colnames(theta.frame)[1] <- "identifier"
 rownames(theta.frame) <- output_names
 write.table(theta.frame, file = 'Arabic_tab/theta.csv', append = FALSE, quote = FALSE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = TRUE, col.names = TRUE)
+
+## stemming
+t1 <- Sys.time()
+stem_dictionary <- sapply(corpus_words, parsing2)
+
+NumberOfForms <- max(unique(sapply(stem_dictionary, length)))
+number_lemmata <- sapply(stem_dictionary, length)
+
+t2 <- Sys.time()
+stemming_time <- t2 - t1
+
+######################################################
+
+## translating
+
+## stemming
+t1 <- Sys.time()
+meaning_dictionary <- sapply(corpus_words, translating)
+
+NumberOfForms <- max(unique(sapply(meaning_dictionary, length)))
+number_lemmata <- sapply(meaning_dictionary, length)
+
+t2 <- Sys.time()
+translating_time <- t2 - t1
+
+#correcting
+
+lemmatiser2 <- function(x){
+  lemmatised <- meaning_dictionary[[x]]
+  return(lemmatised)}
+
+t1 <- Sys.time()
+
+temp <- strsplit(translation_corpus, " ")
+temp_correct <- list()
+for (i in 1:length(temp)) {
+  temp_correct[[i]] <- sapply(temp[[i]], lemmatiser2) 
+}
+NumberOccurrences <- table(unlist(temp_correct))
+
+choose_lemma <- function(x){
+  lemma <- names(which(NumberOccurrences[x]==max(NumberOccurrences[x])))
+  if (length(lemma)==1)
+    return(lemma)}
+
+corrected_corpus <- list()
+for (n in 1:length(temp_correct)) {
+  temp_corrected <- list()
+  counter <- n
+  for (i in 1:length(temp_correct[[counter]])) {
+    temp_corrected[[i]] <- choose_lemma(temp_correct[[counter]][[i]])  
+  }  
+  corrected_corpus[[n]] <- temp_corrected
+}
+
+for (i in 1:length(corrected_corpus)) {
+  corrected_corpus[[i]] <- paste(unlist(corrected_corpus[[i]]), collapse=" ")
+}
+
+translation_corpus <- unlist(corrected_corpus)
+
+doc.list <- strsplit(translation_corpus, "[[:space:]]+")
+t2 <- Sys.time()
+correcting_time <- t2 - t1
+
+# compute the table of terms:
+term.table <- table(unlist(doc.list))
+term.table <- sort(term.table, decreasing = TRUE)
+
+# determing stopwords
+
+stop_words <- as.data.frame(term.table)
+stop_words <- row.names(as.data.frame(stop_words[1:30,]))
+
+# remove terms that are stop words or occur fewer than "occurenses" times:
+occurences <- 3
+del <- names(term.table) %in% stop_words | term.table < occurences
+term.table <- term.table[!del]
+vocab <- names(term.table)
+
+# now put the documents into the format required by the lda package:
+get.terms <- function(x) {
+  index <- match(x, vocab)
+  index <- index[!is.na(index)]
+  rbind(as.integer(index - 1), as.integer(rep(1, length(index))))
+}
+documents <- lapply(doc.list, get.terms)
+
+# Compute some statistics related to the data set:
+D <- length(documents)  # number of documents (2,000)
+W <- length(vocab)  # number of terms in the vocab (14,568)
+doc.length <- sapply(documents, function(x) sum(x[2, ]))  # number of tokens per document [312, 288, 170, 436, 291, ...]
+N <- sum(doc.length)  # total number of tokens in the data (546,827)
+term.frequency <- as.integer(term.table)  # frequencies of terms in the corpus [8939, 5544, 2411, 2410, 2143, ...]
+
+# Fit the model:
+set.seed(seed)
+t1 <- Sys.time()
+fit <- lda.collapsed.gibbs.sampler(documents = documents, K = K, vocab = vocab, 
+                                   num.iterations = G, alpha = alpha, 
+                                   eta = eta, initial = NULL, burnin = 0,
+                                   compute.log.likelihood = TRUE)
+t2 <- Sys.time()
+modelling_time <- t2 - t1
+
+# Visualize
+theta <- t(apply(fit$document_sums + alpha, 2, function(x) x/sum(x)))
+phi <- t(apply(t(fit$topics) + eta, 2, function(x) x/sum(x)))
+
+
+
+translation_corpusAbstracts <- list(phi = phi,
+                                 theta = theta,
+                                 doc.length = doc.length,
+                                 vocab = vocab,
+                                 term.frequency = term.frequency)
+
+
+# create the JSON object to feed the visualization:
+json <- createJSON(phi = translation_corpusAbstracts$phi, 
+                   theta = translation_corpusAbstracts$theta, 
+                   doc.length = translation_corpusAbstracts$doc.length, 
+                   vocab = translation_corpusAbstracts$vocab, 
+                   term.frequency = translation_corpusAbstracts$term.frequency,
+                   R=terms_shown)
+
+#Visulise and start browser
+serVis(json, out.dir = 'ArabicTranslated_vis', open.browser = FALSE)
